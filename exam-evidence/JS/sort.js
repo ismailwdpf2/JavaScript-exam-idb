0f56b1a3c0f90b90c0f9775c3dc79f695079fa4d
Ismail Hosen <118611102+ismailwdpf@users.noreply.github.com>
2023-02-13 00:22:26 +0600

    Add files via upload

---
+++ exam-evidence/JS/sort.js
@@ -0,0 +1,6 @@
+var st = ["Rahim", "Messi", "Ronaldo", "Neimar", "MBappe", "Osil"];
+st.sort();
+// document.write(st)
+st.forEach(e => {
+    document.write(e + "<br>");
+});
---
+++ exam-evidence/JS/split.js
@@ -0,0 +1,6 @@
+function getval() {
+    let val = document.getElementById("d").value;
+    let arr = val.split("-");
+    // console.log(arr);
+    alert(arr[2]);
+}
